7149659864325425b97ee442898349eb2645ed58
ryansteed <rsteed11@gmail.com>
2018-03-08 10:28:24 -0500

    Converted to HTML for blog posts

--- post.php
+++ post.php
@@ -43,7 +43,7 @@
       <div class="container">
         <div class="row">
           <div class="col-lg-8 col-md-10 mx-auto">
-            <?= $post["html"] ?>
+            <?php include_once($postsPath."/".$post["html"]) ?>
           </div>
         </div>
       </div>
