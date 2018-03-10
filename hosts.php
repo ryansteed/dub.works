e53c25d3ad0dc375693f82e335bc29c5364c7066
ryansteed <rsteed11@gmail.com>
2018-03-10 15:25:11 -0500

    Reformatted events page

--- hosts.php
+++ hosts.php
@@ -28,7 +28,7 @@ usort($events, "sortFunction");
     <!-- Header -->
     <section class="text-center" style="padding: 50px;">
       <div class="gallery-container">
-        <div class="row">
+        <div class="row" id="slides">
           <div class="col-lg-12 text-center">
             <h2 class="section-heading text-uppercase" style="color: black">Events</h2>
             <h3 class="section-subheading text-muted" style="margin-bottom: 25px">coming up in Tompkins M06</h3>
@@ -57,7 +57,29 @@ usort($events, "sortFunction");
       </div>
     </section>
 
-    <section id="calendar" class="bg-light">
+    <section class="bg-light" style="padding-top: 50px">
+      <div class="container">
+        <div class="row text-center">
+          <?php
+            foreach($events as $key => $json_data):
+          ?>
+          <div class="col-lg-4 col-md-7 mb-4">
+            <div class="card">
+              <a class="js-scroll-trigger" style="background-color: lightgray" href="#slides"><img class="card-img-top" src="<?= $eventImgUri ?><?= $json_data['image'] ?>" alt=""></a>
+              <div class="card-body">
+                <h4 class="card-title"><h3><?= $json_data["title"] ?></h3></h4>
+                <p class="card-text"><?= $json_data["time"] ?></p>
+                <p class="card-text"><?= $json_data["date"] ?></p>
+                <p class="card-text"><?= $json_data["location"] ?></p>
+              </div>
+            </div>
+          </div>
+          <?php endforeach; ?>
+        </div>
+      </div>
+    </section>
+
+    <section id="calendar">
       <div class="container">
         <div class="row">
           <iframe src="https://calendar.google.com/calendar/b/1/embed?showTitle=0&amp;showPrint=0&amp;showCalendars=0&amp;showTz=0&amp;mode=WEEK&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=csp8dpd2enffr9jn1j9rva2htc%40group.calendar.google.com&amp;color=%23875509&amp;ctz=America%2FNew_York" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
