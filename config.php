356d64ab9e4984666552bb00ca2b2ab9864d6e0a
ryansteed <rsteed11@gmail.com>
2018-03-20 10:11:29 -0400

    readded config file

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+	// Prod Webserver Prefix 
+	$prefixUri   = '/';
+
+	// File path setup
+	$postsPath = "posts";
+	$imgUri = "img/";
+
+	// Development environment
+	// $prefixUri   = '/dub.works/';
+	include_once('common.php');
+?>
