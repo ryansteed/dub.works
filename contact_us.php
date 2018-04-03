057f7b3e2dd036a33c293250c12509c293fc1cec
ryansteed <rsteed11@gmail.com>
2018-04-03 14:20:31 -0400

    Added get involved buttoN

--- contact_us.php
+++ contact_us.php
@@ -4,41 +4,13 @@
     <div class="row">
       <div class="col-lg-12 text-center">
         <h2 class="section-heading text-uppercase">Contact Us</h2>
-        <h3 class="section-subheading text-muted">
+        <h3 class="section-subheading text-muted" style="margin: 50px 0px 0px">
         Want to join the community? Let us know</a> what excites you at <a href="mailto:volunteer@dub.works">volunteer@dub.works</a>.<br/><br/>
           Ideas? Suggestions? Email us at <a href="mailto:contact@dub.works">contact@dub.works</a>.
+          <br><br><br>Want to host an event in our space? Have a project to share?<br><br>
+        <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" target="_blank" href="https://goo.gl/forms/YMe8bEyoNRlVGWzJ2">Get Involved</a>
         </h3>
       </div>
     </div>
-    <!-- Requires EC2 SES @Joseph -->
-    <!-- <div class="row">
-          <div class="col-lg-12">
-            <form id="contactForm" name="sentMessage" novalidate>
-              <div class="row">
-                <div class="col-md-6">
-                  <div class="form-group">
-                    <input class="form-control" id="name" type="text" placeholder="Your Name *" required data-validation-required-message="Please enter your name.">
-                    <p class="help-block text-danger"></p>
-                  </div>
-                  <div class="form-group">
-                    <input class="form-control" id="email" type="email" placeholder="Your Email *" required data-validation-required-message="Please enter your email address.">
-                    <p class="help-block text-danger"></p>
-                  </div>
-                </div>
-                <div class="col-md-6">
-                  <div class="form-group">
-                    <textarea class="form-control" id="message" placeholder="Your Message *" required data-validation-required-message="Please enter a message."></textarea>
-                    <p class="help-block text-danger"></p>
-                  </div>
-                </div>
-                <div class="clearfix"></div>
-                <div class="col-lg-12 text-center">
-                  <div id="success"></div>
-                  <button id="sendMessageButton" class="btn btn-primary btn-xl text-uppercase" type="submit">Send Message</button>
-                </div>
-              </div>
-            </form>
-          </div>
-        </div> -->
   </div>
 </section>
