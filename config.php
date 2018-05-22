7f330056cfc30e1d8842adbe8ecd45738bdfba3e
ryansteed <rsteed11@gmail.com>
2018-05-22 19:20:42 -0400

    Column 14

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
 	// Prod Webserver Prefix 
-	// $prefixUri   = '/';
+	$prefixUri   = '/';
 
 	// File path setup
 	$postsPath = "posts";
 	$imgUri = "img/";
 
 	// Development environment
-	$prefixUri   = '/dub.works/';
+	// $prefixUri   = '/dub.works/';
 	include_once('common.php');
 ?>
